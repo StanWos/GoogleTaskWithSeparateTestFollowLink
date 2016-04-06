0fe3d67aa9b56d7a7bfe257ef40de56b7d842043
standartStan <stas.zoria@gmail.com>
2016-04-06 11:43:50 +0300

    fix

--- src/main/java/pages/GoogleSearchPage.java
+++ src/main/java/pages/GoogleSearchPage.java
@@ -2,21 +2,20 @@ package pages;
 
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static helpers.Helper.sizeOf;
 
 public class GoogleSearchPage {
 
     public static String result = ".srg>.g";
     public static WebDriver driver;
+    public static WebDriverWait wait;
 
     public void followNthLink(int index) {
+        wait.until(sizeOf(By.cssSelector(result), 10));
         driver.findElements(By.cssSelector(result)).get(index).findElement(By.className("r")).click();
-    }
-
-    public void search(String searchText){
-        driver.findElement(By.name("q")).clear();
-        driver.findElement(By.name("q")).sendKeys(searchText + Keys.ENTER);
     }
 
 }
--- src/test/java/com/main/GoogleSearchTest.java
+++ src/test/java/com/main/GoogleSearchTest.java
@@ -1,7 +1,7 @@
 package com.main;
 
 import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,22 +11,21 @@ import pages.GoogleSearchPage;
 import static helpers.Helper.sizeOf;
 import static pages.GoogleSearchPage.result;
 import static pages.GoogleSearchPage.driver;
+import static pages.GoogleSearchPage.wait;
 import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementLocated;
 import static org.openqa.selenium.support.ui.ExpectedConditions.urlContains;
 
 
 public class GoogleSearchTest {
 
-    static WebDriverWait wait;
-
     GoogleSearchPage googlePage = new GoogleSearchPage();
 
-    @BeforeClass
-    public static void setUp() {
+    @Before
+    public void setUp() {
         driver = new FirefoxDriver();
-        wait = new WebDriverWait(driver, 4);
+        wait = new WebDriverWait(driver, 8);
 
-        driver.get("http://google.com/ncr");
+        driver.get("https://www.google.com/?gws_rd=ssl#q=Selenium+automates+browsers");
     }
 
     @AfterClass
@@ -36,14 +35,12 @@ public class GoogleSearchTest {
 
     @Test
     public void testSearch(){
-        googlePage.search("Selenium automates browsers");
         wait.until(sizeOf(By.cssSelector(result), 10));
         wait.until(textToBePresentInElementLocated(By.cssSelector(result + ":nth-child(1)"), "Selenium automates browsers"));
     }
 
     @Test
     public void testSearchAndFollowLink() {
-        googlePage.search("Selenium automates browsers");
         googlePage.followNthLink(0);
         wait.until(urlContains("http://www.seleniumhq.org/"));
     }
